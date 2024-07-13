02c053fb7d616038a7ba5bbbf101d3091c3e8d5a
Foklur <smalkovladislav94@gmail.com>
2024-07-13 09:58:08 +0300

    update code project effects

--- effects/src/components/TimeApi.js
+++ effects/src/components/TimeApi.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 const TimeApi = () => {
     const [time, setTime] = useState(null);
     const [error, setError] = useState(null);
-
+    const [mode, setMode] = useState('short');
     useEffect(() => {
         const fetchTime = async () => {
             try {
@@ -22,11 +22,23 @@ const TimeApi = () => {
         return () => clearInterval(updateTime);
     }, []);
 
+
+    const changeMode = () => {
+        setMode(prevMode => (prevMode === 'short' ? 'extended' : 'short'));
+    }
+
+    const formatTime = time => {
+        if (!time) return 'loading';
+        return mode === 'short' ? time.toLocaleTimeString() : `${time.toLocaleDataString()} ${time.toLocaleTimeString()}`
+    }
+
+
     return (
         <>
             <div>
                 <h1>Time in Kyiv:</h1>
                 {error ? <p>{error}</p> : <h2>{time ? time.toLocaleTimeString() : "Loading"}</h2>}
+                <button onClick={changeMode}>Change to {mode === 'short' ? 'Extended' : 'Short'} View</button>
             </div>
         </>
     );
